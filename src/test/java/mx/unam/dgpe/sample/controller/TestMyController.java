daf8599da40cc5f8a40f7abdd8829aa6ab30b8af
Gustavo Adolfo Arellano Sandoval <garellano@mac.local>
2018-06-04 15:35:57 -0500

    First commit

---
+++ src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -0,0 +1,10 @@
+package mx.unam.dgpe.sample.controller;
+
+import org.apache.log4j.Logger;
+
+import io.vertx.core.AbstractVerticle;
+
+public class TestMyController extends AbstractVerticle {
+    private static final Logger logger = Logger.getLogger(TestMyController.class);
+
+}
