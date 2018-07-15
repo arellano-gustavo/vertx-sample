a4bed9815b1dccf7eacef07dc444c276e51b9026
garellano <garellano@192.168.100.10>
2018-07-14 23:48:20 -0500

    prod version

--- src/main/java/mx/qbits/crypto/executor/controller/MainController.java
+++ src/main/java/mx/qbits/crypto/executor/controller/MainController.java
@@ -1,63 +1,90 @@
 package mx.qbits.crypto.executor.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
-import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.CorsHandler;
 
 public class MainController extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(MainController.class);
-    private static String pba;
     private int port = 6060;
     
     public void start(Future<Void> fut) {
-        logger.info("Inicializando Vertical en puerto: " + port);
+        long start = System.currentTimeMillis();
+        logger.info("Inicializando Vertical de Login en puerto: " + port);
         Router router = Router.router(vertx);
-        router.route("/*").handler(StaticHandler.create("assets"));
-        router.route().handler(BodyHandler.create());//.setUploadsDirectory("upload-folder"));
+        
+        // Este bloque de código es totalmente necesario para evitar el error de CORS ******
+            Set<String> allowedHeaders = new HashSet<>();
+            allowedHeaders.add("x-requested-with");
+            allowedHeaders.add("Access-Control-Allow-Origin");
+            allowedHeaders.add("origin");
+            allowedHeaders.add("Content-Type");
+            allowedHeaders.add("accept");
+            allowedHeaders.add("X-PINGARUNER");
+    
+            Set<HttpMethod> allowedMethods = new HashSet<>();
+            allowedMethods.add(HttpMethod.GET);
+            allowedMethods.add(HttpMethod.POST);
+            allowedMethods.add(HttpMethod.OPTIONS);
+            allowedMethods.add(HttpMethod.DELETE);
+            allowedMethods.add(HttpMethod.PATCH);
+            allowedMethods.add(HttpMethod.PUT);
+    
+            router.route().handler(CorsHandler
+                    .create("*")
+                    .allowedHeaders(allowedHeaders)
+                    .allowedMethods(allowedMethods));
+        // Aqui concluye el código necesrio para evitar el error de CORS ******
+        
+        router.route().handler(BodyHandler.create());          
+        
         router.post("/crypto-trader/bitcoin/coloca").handler(this::doit);
         router.post("/crypto-trader/bitcoin/elimina").handler(this::doit);
         
-        // Create the HTTP server and pass the "accept" method to the request handler.
         vertx.createHttpServer().requestHandler(router::accept).listen( 
                 config().getInteger("http.port", port), result -> {
-                    logger.info("Hola");
+                    logger.info("Hola ---> "+port);
                     if (result.succeeded()) {
                         fut.complete();
                     } else {
                         fut.fail(result.cause());
                     }
                 });        
-        pba = System.getenv("PBA");
-        logger.info("Vertical iniciada !!!" + pba);
+        //pba = System.getenv("PBA");
+        long end = System.currentTimeMillis() - start;
+        logger.info("Vertical de Executor iniciada en " + end + " milisegundos.");
     }  
+    
     private void doit(RoutingContext routingContext) {
         HttpServerResponse response = routingContext.response();
-        HttpServerRequest request = routingContext.request();
-
         String decoded = routingContext.getBodyAsString();
-        String jsonResponse = procesa(decoded, request);
+        String jsonResponse = procesa(decoded);
         response.
-        setStatusCode(200).
-        putHeader("content-type", "application/json; charset=utf-8").
-        end(jsonResponse);
+        setStatusCode(200)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .putHeader("content-type", "application/json")
+        .putHeader("Access-Control-Allow-Origin", "*")
+        .putHeader("Access-Control-Allow-Methods","GET, POST, OPTIONS")
+        .putHeader("Access-Control-Allow-Credentials", "true")
+        .end(jsonResponse);
     }
     
-    private String procesa(String decoded, HttpServerRequest request) {
+    private String procesa(String decoded) {
         Operacion info = Json.decodeValue(decoded, Operacion.class);
         System.out.println(info.getAccion());
         System.out.println(info.getValor());
         System.out.println(info.getCantidad());
+        System.out.println(info.getSender());
         return decoded;
     }
 
--- src/main/java/mx/qbits/crypto/executor/controller/Operacion.java
+++ src/main/java/mx/qbits/crypto/executor/controller/Operacion.java
@@ -4,8 +4,15 @@ public class Operacion {
     private double valor;
     private double cantidad;
     private String accion;
+    private String sender;
     
     public Operacion() {
+    }
+    public String getSender() {
+        return sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
     }
     public double getValor() {
         return valor;
